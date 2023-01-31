05fc2888ac3ba5fc17c4ee8b8030bd67a22d5a50
blueslaboratory <myjunkandspam22@gmail.com>
2023-01-31 16:54:58 +0100

    day 1

--- AndroidStudio/MensajesUsuario/app/src/main/java/com/example/mensajesusuario/MainActivity.java
+++ AndroidStudio/MensajesUsuario/app/src/main/java/com/example/mensajesusuario/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.mensajesusuario;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 
 // 31/01/2023
-// /media/alejandro/METAL SANDISK/IES CLARA/Moviles/AndroidProjects/AndroidStudio/MensajesUsuario/app/build.gradle
+// /media/alejandro/METAL SANDISK/IES CLARA/Moviles/AndroidProjects/AndroidStudio/MensajesUsuario
+// /app/build.gradle
 // cambiar el 32 por el 33
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView myImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    // Boton Toast
     public void verToast(View view) {
 
         LayoutInflater myInflter = getLayoutInflater();
@@ -34,8 +40,6 @@ public class MainActivity extends AppCompatActivity {
         myToast.setView(myLayout);
         myToast.show();
 
-
-
         /*
         Context context = getApplicationContext();
         String text = "Hello toast!";
@@ -45,4 +49,97 @@ public class MainActivity extends AppCompatActivity {
 
         */
     }
+
+
+
+    // Boton Alerta
+    public void myAlert(View vista){
+        AlertDialog.Builder MyAlert = new AlertDialog.Builder(this);
+
+        MyAlert.setTitle("CLASE PMDM DAW+DAM");
+        MyAlert.setMessage("Aprendiendo a poner AlertDialog");
+
+
+        // al dar al boton perfecto llama a paquete y meteme a patricio2
+        MyAlert.setPositiveButton("Perfecto", null);
+        MyAlert.setNegativeButton("No me entero", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                paquete();
+            }
+        });
+        MyAlert.setNeutralButton("A ver si ahora", null);
+
+
+        AlertDialog dialog = MyAlert.create();
+
+        dialog.show();
+    }
+
+    private void paquete(){
+        myImageView = findViewById(R.id.iv1);
+        myImageView.setImageResource( R.drawable.patricio2);
+    }
+
+
+
+    // Boton alerta de Futbol
+    // /media/alejandro/METAL SANDISK/IES CLARA/Moviles/AndroidProjects/AndroidStudio/MensajesUsuario
+    // /app/src/main/res/values/strings.xml
+    public void myAlertFutbol(View vista){
+        AlertDialog.Builder MyAlert = new AlertDialog.Builder(this);
+
+        MyAlert.setTitle("CLASE PMDM DAW+DAM");
+
+
+        // al dar al boton perfecto llama a paquete y meteme a patricio2
+        MyAlert.setItems(R.array.deportes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // i indica la posición del array clickeada
+                pinta_deporte(i);
+            }
+        });
+
+        AlertDialog dialog = MyAlert.create();
+        dialog.show();
+    }
+
+
+    private void pinta_deporte(int i){
+        Context context = getApplicationContext();
+        String text = null;
+
+        switch (i){
+            case 0:{
+                text = "Opcion 1 - Futbol";
+                break;
+            }
+
+            case 1:{
+                text = "Opcion 2 - Futbolin";
+                break;
+            }
+
+            case 2:{
+                text = "Opcion 3 - Futbol sala";
+                break;
+            }
+
+            case 3:{
+                text = "Opcion 4 - Futbol 7";
+                break;
+            }
+        }
+
+        int duration = Toast.LENGTH_LONG;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+
+
+
+
+
 }
