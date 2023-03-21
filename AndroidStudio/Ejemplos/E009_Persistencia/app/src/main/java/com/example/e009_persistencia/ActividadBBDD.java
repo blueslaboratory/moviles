c5cb23a6cfc0f7c1cc8eae964d15118bb716b0dc
blueslaboratory <myjunkandspam22@gmail.com>
2023-03-21 18:15:13 +0100

    day 13 - Divisas

---
+++ AndroidStudio/Ejemplos/E009_Persistencia/app/src/main/java/com/example/e009_persistencia/ActividadBBDD.java
@@ -0,0 +1,14 @@
+package com.example.e009_persistencia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadBBDD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_bbdd);
+    }
+}
---
+++ AndroidStudio/Ejemplos/E009_Persistencia/app/src/main/java/com/example/e009_persistencia/AdministradorBBDD.java
@@ -0,0 +1,4 @@
+package com.example.e009_persistencia;
+
+public class AdministradorBBDD {
+}
